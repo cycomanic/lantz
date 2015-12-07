678269f18805d4af04b7a1c53f011c845e6b532c
Jochen Schroeder <jochen.schroeder@rmit.edu.au>
2015-12-07 20:43:59 +1100

    init for yenista drivers

---
+++ lantz/drivers/yenista/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    lantz.drivers.yenista
+    ~~~~~~~~~~~~~~~~
+
+    :company: Yenista
+    :description: External cavity lasers
+    :website: http://www.yenist.com
+
+    ----
+
+    :copyright: 2015 by Lantz Authors, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from tunics import T100SHP
+
+__all__ = ['T100SHP', ]
+
